d4689937815822298a29fc1f37013b599846ae5d
Rushabh Singwi <rushabhsingwi@gmail.com>
2024-09-16 22:27:11 +0530

    Register Teacher to admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Teacher
+
+
 # Register your models here.
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # Customize the columns you want to display in the admin list view
+    search_fields = ('name',)
